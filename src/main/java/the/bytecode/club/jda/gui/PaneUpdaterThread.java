f16b9d2524ba8d6bdd7203b61c16423082d9b6ed
rcx <rcx@users.noreply.github.com>
2016-12-25 18:05:29 -0500

    Render undisplayable characters as \u literals

--- src/main/java/the/bytecode/club/jda/gui/PaneUpdaterThread.java
+++ src/main/java/the/bytecode/club/jda/gui/PaneUpdaterThread.java
@@ -39,7 +39,8 @@ public class PaneUpdaterThread extends Thread {
             panelArea.setCodeFoldingEnabled(true);
             panelArea.setAntiAliasingEnabled(true);
             final RTextScrollPane scrollPane = new RTextScrollPane(panelArea);
-            panelArea.setText(decompiler.decompileClassNode(viewer.cn, b));
+            String decompileResult = decompiler.decompileClassNode(viewer.cn, b);
+            panelArea.setText(stripUndisplayableChars(decompileResult));
             panelArea.setCaretPosition(0);
             panelArea.setEditable(viewer.isPaneEditable(paneId));
             scrollPane.setColumnHeaderView(new JLabel(decompiler.getName() + " Decompiler - Editable: " + panelArea.isEditable()));
@@ -55,5 +56,26 @@ public class PaneUpdaterThread extends Thread {
             if (button != null)
                 button.setEnabled(true);
         }
+    }
+
+    private String stripUndisplayableChars(String s) {
+        StringBuilder result = new StringBuilder();
+        int startIdx = 0, idx = 0;
+        while (idx < s.length()) {
+            char c = s.charAt(idx);
+            if (isUndisplayable(c)) {
+                result.append(s.substring(startIdx, idx));
+                result.append("\\u").append(Integer.toHexString(c));
+                startIdx = idx + 1;
+            }
+            idx++;
+        }
+        if (idx > startIdx)
+            result.append(s.substring(startIdx, idx));
+        return result.toString();
+    }
+
+    private boolean isUndisplayable(char c) {
+        return c >= 255 || c == 127;
     }
 }
