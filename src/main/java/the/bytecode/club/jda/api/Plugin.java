cdcc48b87002d0b97b9876e065f02a4c39877caa
rcx <rcx@users.noreply.github.com>
2017-04-29 23:05:46 -0400

    Save

--- src/main/java/the/bytecode/club/jda/api/Plugin.java
+++ src/main/java/the/bytecode/club/jda/api/Plugin.java
@@ -1,7 +1,11 @@
 package the.bytecode.club.jda.api;
 
+import the.bytecode.club.jda.FileContainer;
+
 public interface Plugin {
     int onGUILoad();
 
     int onExit();
+
+    int onAddFile(FileContainer container);
 }
